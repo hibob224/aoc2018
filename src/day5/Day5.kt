74d946398578bed683008cdfa92658ea88580902
Jonny Macbook <contact@jmiller.xyz>
2018-12-05 10:42:18 +0000

    Day 5

--- src/day5/Day5.kt
+++ src/day5/Day5.kt
@@ -1,10 +1,39 @@
 package day5
 
+import utils.orZero
+import java.io.File
+import java.util.*
+
 fun main(args: Array<String>) {
-    println("Part one: ${solvePartOne()}")
-    println("Parse two: ${solvePartTwo()}")
+    println("Part one: ${Day5.solvePartOne()}")
+    println("Parse two: ${Day5.solvePartTwo()}")
 }
 
-fun solvePartOne(): Int = 0
+object Day5 {
+    private val actualInput = File("src/day5/input.txt").readText()
 
-fun solvePartTwo(): Int = 0
+    fun solvePartOne(input: String = actualInput): Int {
+        val result = ArrayDeque<Char>(actualInput.length)
+
+        for (it in input) {
+            if (result.isNotEmpty()) {
+                val last = result.peek()
+                if (it.equals(last, true) &&
+                        ((it.isUpperCase() && last.isLowerCase()) || (it.isLowerCase() && last.isUpperCase()))) {
+                    result.pop()
+                    continue
+                }
+            }
+            result.push(it)
+        }
+        return result.size
+    }
+
+    fun solvePartTwo(): Int {
+        val results = mutableMapOf<Char, Int>()
+        "abcdefghijklmnopqrstuvwxyz".forEach {
+            results[it] = solvePartOne(actualInput.replace("$it".toRegex(RegexOption.IGNORE_CASE), ""))
+        }
+        return results.minBy { it.value }?.value.orZero()
+    }
+}
---
+++ src/day5/Day5Test.kt
@@ -0,0 +1,17 @@
+package day5
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class Day5Test {
+
+    @Test
+    fun solvePartOne() {
+        assertEquals(9370, Day5.solvePartOne())
+    }
+
+    @Test
+    fun solvePartTwo() {
+        assertEquals(6390, Day5.solvePartTwo())
+    }
+}
